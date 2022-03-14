c828e609258fca9aa9050d71f5062eabe029fb20
Michele Lombardi <michele.lombardi2@unibo.it>
2022-03-14 13:05:16 +0100

    Slighly improved cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,7 @@ if [ "$(git branch --show-current)" = "main" ]; then
     if [ -z "$(git status --porcelain=v1 2>/dev/null)" ]; then
         echo "SEEK AND DESTROY!!!"
         rm *-sol.ipynb
+        rm *\ -\ sol.ipynb
         rm pdfs/*-sol.pdf
         rm -rf *-dev
         rm docker-compose.yml
